fe62ec17bf79920ee2331deeb1c55a0ecaa054a1
biki <ghullu.wicky@gmail.com>
2022-10-29 01:47:40 +1030

    banner and trailer done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './App.css';
 import Row from './Row';
 import requests from './requests';
+import Banner from './Banner';
+
 
 function App() {
   return (
     <div className="App">
+
+      
+      <Banner/>
      <Row 
      title = "BIKIFLIX ORIGINALS" 
      fetchUrl={requests.fetchNetflixOriginals}
-     isLargeRow
+     isLargeRow={true}
      />
      <Row title= "Trending Now" fetchUrl={requests.fetchTrending}/>
      <Row title= "Top Rated" fetchUrl={requests.fetchTopRated}/>
--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import "./Row.css";
+import Youtube from "react-youtube";
+import movieTrailer from 'movie-trailer';
 
 const base_url ="https://image.tmdb.org/t/p/original/";
 
 function Row({title, fetchUrl, isLargeRow})  {
     const [movies, setMovies] = useState([]);
-    
+    const [trailerUrl, setTrailerUrl] = useState("");
     useEffect(()=> {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
@@ -19,9 +21,30 @@ function Row({title, fetchUrl, isLargeRow})  {
   
     }, [fetchUrl]);
 
+const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+        autoplay:1,
 
+    },
+};
 
-   console.table(movies);
+const handleClick = (movie) => {
+    if (trailerUrl){
+        setTrailerUrl("");
+    }
+    else{
+   
+    movieTrailer(movie?.name || "" )
+    .then((url) => {
+        const urlParams= new URLSearchParams(new URL(url).search);
+        setTrailerUrl(urlParams.get("v"));
+    })
+    .catch((error) => console.log(error));
+    }
+    };
+
 
 
 
@@ -35,13 +58,16 @@ function Row({title, fetchUrl, isLargeRow})  {
                 {movies.map(movie=>(
                     <img
                     key={movie.id}
-                    className= {`row_poster` ${isLargeRow && "row_posterLarge"}
-                    src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path }`} 
+                    onClick={() => handleClick(movie)}
+                    className= {`row_poster ${isLargeRow && "row_posterLarge"}`}
+                    src={`${base_url}${isLargeRow? movie.poster_path : movie.backdrop_path }`} 
                     alt={movie.name}/>
                 ))}
                 </div>
+                {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
+
         </div>
-    )
+    );
   }
 
 export default Row
